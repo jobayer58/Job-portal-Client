691fdb4ebbfca6907256bf64c35ef0ff135a2365
jobayer58 <jobayerafnan770@gmail.com>
2025-04-10 18:43:39 +0600

    update job portal news

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import Swal from 'sweetalert2';
+import useAuth from '../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const AddJob = () => {
 
+    const {user} =useAuth()
+    const navigate = useNavigate()
+
     const handleAddJob = e => {
         e.preventDefault()
+        const formData = new FormData(e.target)
+        // console.log(formData.entries());
+        const initialData = Object.fromEntries(formData.entries())
+        console.log(initialData);
+        const { min, max, currency, ...newJob } = initialData
+        console.log(newJob);
+        newJob.salaryRange = { min, max, currency }
+        newJob.requirements = newJob.requirements.split('\n')
+        newJob.responsibilities = newJob.responsibilities.split('\n')
+        console.log(newJob);
+
+        fetch('http://localhost:5000/jobs', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newJob)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Job Has Been Added",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    navigate('/myPostedJobs')
+                }
+            })
+
     }
     return (
         <div>
@@ -19,16 +57,16 @@ const AddJob = () => {
                         <input type="text" name='location' className="input w-full" placeholder="Job Location" />
                         {/* job type*/}
                         <label className="fieldset-label">Job Type</label>
-                        <select className="select w-full">
-                            <option disabled selected>Pick a Job Type</option>
+                        <select defaultValue='Pick a Job Type' name='JobType' className="select w-full">
+                            <option disabled >Pick a Job Type</option>
                             <option>Full-Time</option>
                             <option>Intern</option>
                             <option>Part-Time</option>
                         </select>
                         {/* job type*/}
                         <label className="fieldset-label">Job Field</label>
-                        <select className="select w-full">
-                            <option disabled selected>Pick a Job Field</option>
+                        <select defaultValue='Pick a Job Field' name='jobField' className="select w-full">
+                            <option disabled >Pick a Job Field</option>
                             <option>Engineering</option>
                             <option>Finance</option>
                             <option>Finance</option>
@@ -44,8 +82,8 @@ const AddJob = () => {
                                 <input type="text" name='max' className="input w-full" placeholder="Max" />
                             </div>
                             <div>
-                                <select className="select w-full">
-                                    <option disabled selected>Currency</option>
+                                <select defaultValue='Currency' name='currency' className="select w-full">
+                                    <option disabled >Currency</option>
                                     <option>BDT</option>
                                     <option>USD</option>
                                     <option>INR</option>
@@ -69,7 +107,10 @@ const AddJob = () => {
                         <input type="text" name='hr_name' className="input w-full" placeholder="HR name" required />
                         {/* HR Email */}
                         <label className="fieldset-label">HR Email</label>
-                        <input type="text" name='hr_email' className="input w-full" placeholder="HR Email" required />
+                        <input defaultValue={user?.email} type="text" name='hr_email' className="input w-full" placeholder="HR Email" required />
+                        {/* Application Deadline */}
+                        <label className="fieldset-label">Application Deadline</label>
+                        <input type="date" name='applicationDeadline' className="input w-full" placeholder="Deadline" required />
                         {/* Company Logo URL */}
                         <label className="fieldset-label">Company Logo URL</label>
                         <input type="text" name='company_logo' className="input w-full" placeholder="Company Logo URL" required />
---
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
+const ViewApplications = () => {
+    const application = useLoaderData()
+
+    const handleStatusUpdate = (e, id) => {
+        console.log(e.target.value, id);
+        const data = {
+            status: e.target.value
+        }
+        fetch(`http://localhost:5000/job-applications/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Status Has Been Updated",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    
+                }
+            })
+    }
+    return (
+        <div>
+            <h2 className="text-3xl">Application for this job: {application.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Email</th>
+                            <th>Status</th>
+                            <th>Update Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            application.map((app, index) => <tr key={app._id}>
+                                <th>{index + 1}</th>
+                                <td>{app.applicant_email}</td>
+                                <td>Quality control specialist</td>
+                                <td>
+                                    <select
+                                        onChange={(e) => handleStatusUpdate(e, app._id)}
+                                        defaultValue={app.status || 'Change Status'}
+                                        className="select select-xs">
+                                        <option disabled={true}>Change Status</option>
+                                        <option>Under Review</option>
+                                        <option>Set Interview</option>
+                                        <option>Hired</option>
+                                        <option>Rejected</option>
+                                    </select>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default ViewApplications;
